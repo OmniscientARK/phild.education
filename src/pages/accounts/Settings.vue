10098d462fe576f96af7d693bab23bf02948d624
OmniscientARK <lorenzopiuga@gmail.com>
2021-05-08 12:44:48 -0300

    Changes by Russian-Foguete

---
+++ src/pages/accounts/Settings.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id = "Happy">
+    <h1>Happy</h1>
+    <p>
+      Happy
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Settings",
+  data() {
+    return {};
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
