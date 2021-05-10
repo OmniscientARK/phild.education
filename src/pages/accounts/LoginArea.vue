ddb68f2cb6754208864c18c64618bf13bb6e22ee
OmniscientARK <lorenzopiuga@gmail.com>
2021-05-10 14:04:24 -0300

    -

---
+++ src/pages/accounts/LoginArea.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="profile">
+    <Navbar app="Accounts">
+    </Navbar>
+    <form @submit.prevent="loginHandler">
+      <input type="text" id="login" v-model="username" placeholder="Usuário ou email" autocomplete="off">
+      <input type="password" id="password" v-model="password" placeholder="Senha" autocomplete="off">
+      <input type="submit" value="Login">
+    </form>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/pages/other/nav/Navbar";
+import axios from "axios";
+
+export default {
+  name: "LoginArea",
+  components:{
+    Navbar
+  },
+  data(){
+    return{
+      username: "",
+      password: "",
+    }
+  },
+  methods: {
+    loginHandler(){
+      console.log("loginHandler")
+      axios.get("http://189.4.173.228:81/?c=login").then((response) => {
+        console.log(response)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/accounts/Profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="profile">
+    <Navbar app="Accounts">
+      <SearchBar/>
+    </Navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/pages/other/nav/Navbar";
+import SearchBar from "@/pages/other/nav/elements/SearchBar";
+
+export default {
+  name: "Profile",
+  components: {
+    SearchBar,
+    Navbar
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
